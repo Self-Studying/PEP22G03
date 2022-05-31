ac6ad53324014aa36c32261367c5d6a1d08d8b0c
stefan <stefanversan@gmail.com>
2022-05-31 21:32:50 +0300

    First Commit

--- Modul1/part1.py
+++ Modul1/part1.py
@@ -2,9 +2,26 @@ print("hello world!\n", 'My Object', end="#", sep="-abcd-", )
 
 print('print')
 
-print("###\n"
-      "# #\n"
-      "###")
+print("############################\n"
+      "#                          #\n"
+      "############################")
 print(type(print))
 print(type(3))
 print(type('3'))
+print(3 + 3)
+print("3" + "3")
+print("3" * 5)
+print("#" * 30, "\n", "#                         #\n", "#" * 30)
+
+print("#" * 30)
+print(sep=" ")
+print("#" * 28)
+print("#" * 30)
+
+my_value = "#" * 30
+my_value1 = "#" + ' '*28 + "#"
+print(my_value, my_value1, my_value, sep='\n')
+
+
+
+print("#" * 30, "#"," " * 28,sep=""  ,"#" * 30)
