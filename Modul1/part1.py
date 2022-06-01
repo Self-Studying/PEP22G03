83d456c539e08092eeedcedc205e7aba8068a868
stefan <stefanversan@gmail.com>
2022-06-01 07:18:34 +0300

    Second Commit

--- Modul1/part1.py
+++ Modul1/part1.py
@@ -19,9 +19,8 @@ print("#" * 28)
 print("#" * 30)
 
 my_value = "#" * 30
-my_value1 = "#" + ' '*28 + "#"
+my_value1 = "#" + ' ' * 28 + "#"
 print(my_value, my_value1, my_value, sep='\n')
 
 
-
-print("#" * 30, "#"," " * 28,sep=""  ,"#" * 30)
+print("#" * 30, "#" + ' ' * 28 + "#", "#" * 30, sep="\n")
