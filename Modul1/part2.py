39400695d424505eb11ea487e5989a77c550d52b
stefan <stefanversan@gmail.com>
2022-06-01 16:36:16 +0300

    Second Commit

--- Modul1/part2.py
+++ Modul1/part2.py
@@ -1,4 +1,18 @@
 x = 3  # hardcode your test data here
 x = float(x)
-y = 3*x**3 - 2*x**2 + 3*x -1# write your code here
+y = 3 * x ** 3 - 2 * x ** 2 + 3 * x - 1  # write your code here
 print("y =", y)
+
+# this program counts the number of seconds in a given hour
+# this program has been written yestersay
+
+a = 2  # assigning a value to our variable equal to two hours
+seconds = 3600  # number of seconds in onwe hour
+hour = 1800
+print("Hours: ", a, hour, end="\n", sep=",")  # printing the number of hours
+# print("Seconds in Hours: ", a * seconds) # printing the number of seconds in a given number of hours
+
+# here we should also
+print("Goodbye")
+# but a programmer didn't have time to write any code
+# EOF that computes the number of seconds in 3 hour
