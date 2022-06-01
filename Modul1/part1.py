48609187c59a47bab2d80d9b6b7f665daffa16c6
stefan <stefanversan@gmail.com>
2022-06-01 16:22:21 +0300

    Sixth  Commit

--- Modul1/part1.py
+++ Modul1/part1.py
@@ -102,3 +102,16 @@ a = 3.0
 b = 4.0
 c = (a ** 2 + b ** 2) ** 0.5
 print("c =", c)
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = 7.38 + 12 * 1.61
+kilometers_to_miles = 12 / 1.61
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+x = 3  # hardcode your test data here
+x = float(x)
+y = 3*x**3 - 2*x**2 + 3*x -1# write your code here
+print("y =", y)
