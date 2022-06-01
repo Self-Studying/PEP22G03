d2ecd92207a2ea1d6c221590c82b5db0ec6a5e74
stefan <stefanversan@gmail.com>
2022-06-01 12:30:49 +0300

    Third Commit

--- Modul1/part1.py
+++ Modul1/part1.py
@@ -1,5 +1,7 @@
+import datetime
+from termcolor import colored
 print("hello world!\n", 'My Object', end="#", sep="-abcd-", )
-
+print(datetime.datetime.now())
 print('print')
 
 print("############################\n"
@@ -21,6 +23,27 @@ print("#" * 30)
 my_value = "#" * 30
 my_value1 = "#" + ' ' * 28 + "#"
 print(my_value, my_value1, my_value, sep='\n')
-
+print("my name", "is", end="")
 
 print("#" * 30, "#" + ' ' * 28 + "#", "#" * 30, sep="\n")
+print("My", 'name', "is", "Python", "Monty", "Python.", sep="-")
+print("My", 'name', "is", "Python", "Monty", "Python.", sep="")
+print("My", 'name', "is", "Python", "Monty", "Python.", sep=" ")
+print()
+print("My", "Name", "is", sep="_", end="*")
+print("Monty", "Python", sep="*", end="*\n")
+print("Programming***Essentials***in...",end="")
+print("Python")
+
+
+print(colored("    *", "magenta"))
+print(colored("  * *  *","magenta"))
+print(colored(" *  *   *", "magenta"))
+print(colored("*   *    *", "magenta"))
+print(colored("****    ***", "magenta"))
+print(colored("  *  * *", "magenta"))
+print(colored("  *  * *", "magenta"))
+print(colored("  ******", "magenta"))
+print(.4)
+print(3E8)
+print(0.000000000000000000000003)
