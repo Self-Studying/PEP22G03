f0be20923ae0ed3fb8c5548a4364217328376093
stefan <stefanversan@gmail.com>
2022-06-03 10:43:40 +0300

    First Commit

---
+++ Modul3/loops1.py
@@ -0,0 +1,21 @@
+import time
+
+for i in range(13):
+    print("The current range is ", i)
+
+for i in range(2, 8, 3):
+    print("The value of i is currently", i)
+
+# Write a for loop that counts to five.
+for i in range(1, 6):
+    if i == range:
+        print(i)
+    else:
+        print('Mississippi')
+    continue
+
+
+# Body of the loop - print the loop iteration number and the word "Mississippi".
+# Body of the loop - use: time.sleep(1)
+
+# Write a print function with the final message.
