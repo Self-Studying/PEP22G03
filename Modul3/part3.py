9a7f8e1d9878163d7e7b93eda7bc8ad6e2eb86c3
stefan <stefanversan@gmail.com>
2022-06-03 08:33:52 +0300

    Third Commit

--- Modul3/part3.py
+++ Modul3/part3.py
@@ -5,9 +5,24 @@ number3 = int(input("Enter the third number: "))
 
 # Check which one of the numbers is the greatest
 # and pass it to the largest_number variable.
-
+import os, sys
 largest_number = max(number1, number2, number3)
 smallest_number = min(number1, number2, number3)
 # Print the result.
 print("The largest number is:", largest_number)
 print("The smallest number is:", smallest_number)
+
+
+my_flower = "Spathiphyllum"
+cmd = input()
+if cmd == cmd.lower():
+    print("No, I want a big Spathiphyllum!")
+elif input == "palargonium":
+    print("Spathiphyllum! Not palargonium!")
+else:
+    var = my_flower == input("Spathiphyllum")
+    print("Yes - Spathiphyllum is the best plant ever!")
+
+
+
+
