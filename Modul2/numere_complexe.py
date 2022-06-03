3d8d42504a33abd9d39389d19dac5be573315ad7
stefan <stefanversan@gmail.com>
2022-06-03 07:44:59 +0300

    Third Commit

--- Modul2/numere_complexe.py
+++ Modul2/numere_complexe.py
@@ -3,5 +3,15 @@ number1 = 3J
 
 print(type(number1))
 
-number3 = (-1)**(1/2)
+number3 = (-1) ** (1 / 2)
 print(number3)
+
+print(bool(-100), bool(0.1), bool(0 + 2))
+a = 2
+b = 4
+print("True or 1 is : ", True.__xor__(1))
+print("a" .__add__("b"))
+print("a" + "b")
+a = 3
+v = 5
+print(a .__sub__(b))
