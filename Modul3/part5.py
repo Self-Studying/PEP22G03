41ef042b8b919f7a49fc7930764d5a19d3b77dd7
stefan <stefanversan@gmail.com>
2022-06-03 09:17:35 +0300

    Fifth Commit

--- Modul3/part5.py
+++ Modul3/part5.py
@@ -18,3 +18,43 @@ if x > 5:  # True
         print("nested: else")
 else:
     print("else")
+
+x = 10
+
+if x == 10:
+    print(x == 10)
+if x > 5:
+    print(x > 5)
+if x < 10:
+    print(x < 10)
+else:
+    print("else")
+
+x = 1
+y = 1.0
+z = "1"
+
+if x == y:
+    print("one")
+if y == int(z):
+    print("two")
+elif x == y:
+    print("three")
+else:
+    print("four")
+
+x = "1"
+
+if x == 1:
+    print("one")
+elif x == "1":
+    if int(x) > 1:
+        print("two")
+    elif int(x) < 1:
+        print("three")
+    else:
+        print("four")
+if int(x) == 1:
+    print("five")
+else:
+    print("six")
