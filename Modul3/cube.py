ef422e559076970ce843b0ec0d91f554b969802c
stefan <stefanversan@gmail.com>
2022-06-07 19:02:43 +0300

    Fourth Commit

--- Modul3/cube.py
+++ Modul3/cube.py
@@ -4,10 +4,18 @@ while i > 2:
     i -= 2
 print(i)
 
-
-for i in range(-1,1):
+for i in range(-1, 1):
     print("#")
 
-my_list = [3,1,-1]
+my_list = [3, 1, -1]
 my_list[-1] = my_list[-2]
 print(my_list)
+
+my_list = "Python is Great!"
+print(my_list[1:6:2])
+
+"""-> start slice; end slice; pas slice[literele,caracterele care se vor omite]"""
+print(len(my_list))
+print(id(str))
+print(my_list[:18])
+print('King of Sun is :', my_list[11::2])
