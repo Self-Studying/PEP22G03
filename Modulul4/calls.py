339bf92423cf9746f9e4d7fa14f38a955696313e
stefan <stefanversan@gmail.com>
2022-06-27 09:38:31 +0300

    First Commit

--- Modulul4/calls.py
+++ Modulul4/calls.py
@@ -7,4 +7,13 @@ def happy_new_year(wishes=True):
     print("Happy New Year!,2023")
 
 
+def happy_new_year2023(wishes=False):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+        return
+    print("Happy New Year!,2023")
+
+
 happy_new_year()
