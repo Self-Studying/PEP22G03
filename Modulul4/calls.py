cea1e80fac0657d6a97767a4eacfcf4e732e3190
stefan <stefanversan@gmail.com>
2022-06-27 09:36:45 +0300

    ThirdCommit 27.06.2022

---
+++ Modulul4/calls.py
@@ -0,0 +1,10 @@
+def happy_new_year(wishes=True):
+    print("Three...")
+    print("Two...")
+    print("One...")
+    if not wishes:
+        return
+    print("Happy New Year!,2023")
+
+
+happy_new_year()
